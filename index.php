b18bae6c2247fa11bab7c420d4cea85c5cd29716
Orhan AYDOĞDU <info@orhanaydogdu.com.tr>
2020-03-19 02:52:04 +0300

    v1.0.2
    :) :+1:

--- index.php
+++ index.php
@@ -5,19 +5,8 @@
     
     include("env.php");
 
-    $main_response = array(
-        "status" => false,
-        "desc" => "",
-        "result" => array(
-            "total"=>0,
-            "death"=>0,
-            "recovered"=>0,
-            "data"=>[]
-        )
-    );
-    $data = file_get_contents($_ENV["api_url" ]);
-
     function sort_data($data, $orderby){
+        global $main_response;
         $sort_list=[
             'total',
             'death',
@@ -35,10 +24,24 @@
                 } 
             }
             array_multisort($sortArray[$orderby], SORT_DESC, $data);
+            $main_response['desc'] = "Sort Ok";
         }
-        
+
         return $data;
     }
+
+    
+    $main_response = array(
+        "status" => false,
+        "desc" => "",
+        "result" => array(
+            "total"=>0,
+            "death"=>0,
+            "recovered"=>0,
+            "data"=>[]
+        )
+    );
+    $data = file_get_contents($_ENV["api_url" ]);
 
     if($data){
         $data = json_decode($data);
@@ -48,13 +51,19 @@
         }
 
         $main_response['result']['data'] = $data;
+
         foreach($data as $case){
             $main_response['result']['total'] = $main_response['result']['total']+$case->total;
             $main_response['result']['death'] = $main_response['result']['death']+$case->death;
             $main_response['result']['recovered'] = $main_response['result']['recovered']+$case->recovered;
         }
+
         $main_response['status'] = true;
-        $main_response['desc'] = "OK";
+
+        if($main_response['desc'] === ""){
+            $main_response['desc'] = "OK";
+        }
+
     }else{
         $main_response['desc'] = "Please try again later";
     }
